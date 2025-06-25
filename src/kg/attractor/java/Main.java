3982484079a765326594cae1d57da473d8f07b82
nargiza-jd <supabaevanargiza@gmail.com>
2025-06-25 17:50:05 +0600

    Start of lesson45: authorization, login.ftlh, Lesson45Server

--- src/kg/attractor/java/Main.java
+++ src/kg/attractor/java/Main.java
@@ -1,13 +1,14 @@
 package kg.attractor.java;
 
 import kg.attractor.java.lesson44.Lesson44Server;
+import kg.attractor.java.lesson44.Lesson45Server;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            new Lesson44Server("localhost", 8089).start();
+            new Lesson45Server("localhost", 8089).start();
         } catch (IOException e) {
             e.printStackTrace();
         }
