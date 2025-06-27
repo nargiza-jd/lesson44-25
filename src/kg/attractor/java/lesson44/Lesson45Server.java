932d1d21c7eef1af90abe21dd6efe6de2c438433
nargiza-jd <supabaevanargiza@gmail.com>
2025-06-27 16:13:56 +0600

    Implemented authorization at /login and removed index.html

--- src/kg/attractor/java/lesson44/Lesson45Server.java
+++ src/kg/attractor/java/lesson44/Lesson45Server.java
@@ -3,9 +3,12 @@ package kg.attractor.java.lesson44;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import kg.attractor.java.server.ContentType;
+import kg.attractor.java.server.ResponseCodes;
+import kg.attractor.java.utils.Utils;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Map;
 
 public class Lesson45Server extends Lesson44Server{
     public Lesson45Server(String host, int port) throws IOException {
@@ -18,7 +21,18 @@ public class Lesson45Server extends Lesson44Server{
         String cType = getContentType(exchange);
         String raw = getBody(exchange);
 
+        Map<String, String> parsed = Utils.parseUrlEncoded(raw, "&");
 
+        String fmt = "<p>Необработанные данные: <b>%s</b></p>" +
+                "<p>Content-Type: <b>%s</b></p>" +
+                "<p>После обработки: <b>%s</b></p>";
+        String data = String.format(fmt, raw, cType, parsed);
+
+        try {
+            sendByteData(exchange, ResponseCodes.OK, ContentType.TEXT_HTML, data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void loginGet(HttpExchange exchange) {
