ceadc2d5ac6efe21b62c62544b1d949e0b824207
nargiza-jd <supabaevanargiza@gmail.com>
2025-07-01 21:52:58 +0600

    Changed Book class and books.json

--- src/kg/attractor/java/model/Book.java
+++ src/kg/attractor/java/model/Book.java
@@ -5,20 +5,20 @@ public class Book {
     private String title;
     private String author;
     private String image;
+    private String description;
     private BookStatus status;
     private String issuedTo;
-
     private String holderEmail;
 
-    public Book(String id, String title, String author, String image, BookStatus status, String issuedTo) {
+    public Book(String id, String title, String author, String image, String description, BookStatus status, String issuedTo) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.image = image;
+        this.description = description;
         this.status = status;
         this.issuedTo = issuedTo;
     }
-
 
     public String getHolderEmail() {
         return holderEmail;
@@ -42,6 +42,10 @@ public class Book {
 
     public String getImage() {
         return image;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public BookStatus getStatus() {
@@ -68,6 +72,10 @@ public class Book {
         this.image = image;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     public void setStatus(BookStatus status) {
         this.status = status;
     }
@@ -83,6 +91,7 @@ public class Book {
                 ", title='" + title + '\'' +
                 ", author='" + author + '\'' +
                 ", image='" + image + '\'' +
+                ", description='" + description + '\'' +
                 ", status=" + status +
                 ", issuedTo='" + issuedTo + '\'' +
                 '}';
